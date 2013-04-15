f6c6a1da9a8b5f65723ff7c761bc4b4da114a44f
Jura Khrapunov <jura@khrapunov.com>
2013-04-15 14:35:23 +0200

    Added Yii framework files Use two different credentials sets depend on hosting

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../../programmes/yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
-
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-require_once($yii);
-Yii::createWebApplication($config)->run();
+if ( in_array($_ENV('SERVER_ADDR'), array('::1', '127.0.0.1')) ) {
+	// change the following paths if necessary
+	$yii=dirname(__FILE__).'/../../programmes/yii/framework/yii.php';
+	$config=dirname(__FILE__).'/protected/config/local.php';
+	
+	// remove the following lines when in production mode
+	defined('YII_DEBUG') or define('YII_DEBUG',true);
+	// specify how many levels of call stack should be shown in each log message
+	defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+	
+	require_once($yii);
+	Yii::createWebApplication($config)->run();
+	
+} elseif(isset($_ENV['CRED_FILE'])) {
+	
+	// change the following paths if necessary
+	$yii=dirname(__FILE__).'/protected/framework/yiilite.php';
+	$config=dirname(__FILE__).'/protected/config/cloudcontrolled.php';
+	
+	// remove the following lines when in production mode
+	// defined('YII_DEBUG') or define('YII_DEBUG',true);
+	// specify how many levels of call stack should be shown in each log message
+	//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+	
+	require_once($yii);
+	Yii::createWebApplication($config)->run();
+	
+}
---
+++ protected/config/cloudcontrolled.php
@@ -0,0 +1,113 @@
+<?php
+// this config file is specific for CloudControl hosting
+
+# read the credentials file
+$string = file_get_contents($_ENV['CRED_FILE'], false);
+if ($string == false) {
+	die('FATAL: Could not read credentials file');
+}
+
+# the file contains a JSON string, decode it and return an associative array
+$creds = json_decode($string, true);
+
+
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'Ligo',
+	'defaultController' => 'people',
+	
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+		'ext.YiiMongoDbSuite.*',
+		'ext.Codebird.*',
+	),
+
+	'modules'=>array(
+		// uncomment the following to enable the Gii tool
+		/*
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'Enter Your Password Here',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
+		*/
+	),
+
+	// application components
+	'components'=>array(
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+		),
+		// uncomment the following to enable URLs in path-format
+		
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'showScriptName'=>false,
+			'rules'=>array(
+				//'' => 'site/index',
+				//'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				//'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+				//'<action:\w+>' => 'site/<action>',
+			),
+		),
+		
+		/*'db'=>array(
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+		),*/
+		
+		// uncomment the following to use a MySQL database
+		/*
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => '',
+			'charset' => 'utf8',
+		),
+		*/
+		'mongodb' => array(
+				'class'            => 'EMongoDB',
+				'connectionString' => $creds['MONGOlAB']['MONGOLAB_URI'],
+				//'dbName'           => 'ligo',
+		),
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>'site/error',
+		),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*array(
+					'class'=>'CWebLogRoute',
+					'showInFireBug' => true,
+				),*/
+			),
+		),
+	),
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>array(
+		// this is used in contact page
+		'adminEmail'=>'jura.khrapunov@undp.org',
+	),
+);
